f90e0203a36bc6de35676a6fcdddf701c10b72a1
Jose <diazmarinjoseelias2007@gmail.com>
2023-01-02 16:18:04 -0500

    knowty

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,29 +14,29 @@ export const Footer = () => {
         <div className="textFooter 	d-none d-md-block d-lg-block">
           <h1>Knowty</h1>
           <div className="d-flex">
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Jose Díaz</h4>
               <p>Creador del proyecto, programador de la página y creador de contenido para YouTube</p>
               <AiFillYoutube size={36} className="position-absolute bottom-0 end-0 m-1" />
               <FaDev size={34} className="position-absolute bottom-0 end-0 m-1 me-5" />
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Wilmar Fontalvo</h4>
               <p>Creador de videos y encargado principal de YouTube</p>
               <AiFillYoutube size={36} className="position-absolute bottom-0 end-0 m-1" />
 
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Yader Vega</h4>
               <p>Encargado del marketing y contribuidor al diseño de la página</p>
               <BsMegaphoneFill size={34} className="position-absolute bottom-0 end-0 m-2" />
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Mauricio Lopez</h4>
               <p>Principal encargado del marketing de la página y proyecto</p>
               <BsMegaphoneFill size={34} className="position-absolute bottom-0 end-0 m-2" />
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>David Cáceres</h4>
               <p>Encargado de la creación de videos para el canal de YouTube</p>
               <AiFillYoutube size={36} className="position-absolute bottom-0 end-0 m-1" />
@@ -47,19 +47,19 @@ export const Footer = () => {
         <div className="textFooter d-block d-sm-none">
           <h1>Knowty</h1>
           <div className="d-flex">
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>David Cáceres</h4>
               <p>Encargado de la creación de videos para el canal de YouTube</p>
               <AiFillYoutube size={36} className="position-absolute bottom-0 end-0 m-1" />
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Wilmar Fontalvo</h4>
               <p>Creador de videos y encargado principal de YouTube</p>
               <AiFillYoutube size={36} className="position-absolute bottom-0 end-0 m-1" />
             </div>
           </div>
           <div>
-            <div className="teamCardCenter shadow pb-2" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCardCenter ownShadow pb-2" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Jose Díaz</h4>
               <p>Creador del proyecto, programador de la página y creador de contenido para YouTube</p>
               <FaDev size={32} className="position-absolute bottom-0 end-0 m-1 me-5" />
@@ -67,12 +67,12 @@ export const Footer = () => {
             </div>
           </div>
           <div className="d-flex">
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Yader Vega</h4>
               <p>Encargado del marketing y contribuidor al diseño de la página</p>
               <BsMegaphoneFill size={34} className="position-absolute bottom-0 end-0 m-2" />
             </div>
-            <div className="teamCard shadow" data-aos="zoom-in-down" data-aos-duration="1400" >
+            <div className="teamCard ownShadow" data-aos="zoom-in-down" data-aos-duration="1400" >
               <h4>Mauricio Lopez</h4>
               <p>Principal encargado del marketing de la página y proyecto</p>
               <BsMegaphoneFill size={34} className="position-absolute bottom-0 end-0 m-2" />
@@ -80,7 +80,7 @@ export const Footer = () => {
           </div>
         </div>
 
-        <div className="moreInformation shadow" data-aos="fade-down" data-aos-duration="1400" >
+        <div className="moreInformation ownShadow" data-aos="fade-down" data-aos-duration="1400" >
           <h3>¿Quieres saber más?</h3>
           <div className="flex-1 justify-content-evenly">
             <div className="mx-2">
--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -2,7 +2,7 @@ import "./SectionTitle.css"
 
 export const SectionTitle = ({title}) => {
     return (
-        <div className="textIntroduction shadow-lg">
+        <div className="textIntroduction ownShadow-lg">
             <h1>{title}</h1>
         </div>
     )
