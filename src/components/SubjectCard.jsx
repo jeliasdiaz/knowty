d632ebd205228c89c788ed955712e50e32549222
Jose <diazmarinjoseelias2007@gmail.com>
2023-02-13 10:38:46 -0500

    knowty

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -6,7 +6,7 @@ const SubjectCard = ({ path, img, title, marginTop, animationDuration }) => {
         <>
         <Link to={path} className="text-decoration-none text-black d-none d-sm-block" data-aos="fade-down" data-aos-duration={animationDuration} data-aos-once="true">
             <div className={`subjectLink d-flex ownShadow ${marginTop} animation`}>
-                <img src={`/img/${img}Icon.png`} alt="Subject img" className="w-25 pe-2 animation" />
+                <img src={`/img/${img}Icon.png`} alt="Subject img" className=" pe-2 animation" />
                 <h5 className="ms-2 animation">{title}</h5>
                 <BiChevronRight size={45} className="BiChevronRight animation" />
             </div>
