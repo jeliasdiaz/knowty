3c466d473e646d09c23567c20ef3c813cf280f71
Jose <diazmarinjoseelias2007@gmail.com>
2023-04-15 21:28:21 -0500

    knowty

--- src/pages/Biology/LifeAndCells.jsx
+++ src/pages/Biology/LifeAndCells.jsx
@@ -25,8 +25,57 @@ export const LifeAndCells = () => {
                     A partir de esto, los seres vivos pueden clasificarse según la cantidad de células que poseen, ya sea unicelular (una célula) o pluricelular (dos o más células). Sin embargo, ambos tipos caracterizados por poseer en su interior organelos especializados en cumplir funciones específicas para el correcto funcionamiento de la célula. Aunque los organelos no sean los mismos para todos los tipos de célula existentes, Eucariotas y Procariotas.
                 </p>
                 <br />
-                <CollapseInformation name="Célula eucariota" id="eucariota">
-                    <p>Son aquellas...</p>
+                <CollapseInformation name="Células eucariotas" id="eucariota">
+                    <p>
+                        Las células eucariotas son células que tienen un núcleo definido y separado del resto de la célula por una membrana nuclear. Además del núcleo, estas células también tienen otros orgánelos celulares, como mitocondrias, retículo endoplásmico, aparato de Golgi y lisosomas, que realizan diversas funciones en la célula. Estas células se encuentran en organismos multicelulares, como plantas, animales y hongos. A menudo son más grandes y complejas que las células procariotas, que carecen de un núcleo definido y otros orgánulos celulares.
+
+                    </p>
+                </CollapseInformation>
+                <hr />
+                <CollapseInformation name="Células procariotas" id="procariota">
+                    <p>
+                        Son células simples y pequeñas que carecen de núcleo y de orgánelos rodeados de membrana. A diferencia de las células eucariotas, encuentradas en plantas, animales, hongos y otros organismos complejos, las células procariotas son típicamente unicelulares y se encuentran en bacterias y arqueas.
+                        <br />
+                        <br />
+                        Además, poseen una estructura celular básica, que incluye una membrana plasmática que rodea el citoplasma, donde se encuentran los ribosomas y el material genético en forma de una molécula circular de ADN. Es de añadir algunas células procariotas pueden tener estructuras adicionales, como flagelos para el movimiento y pili para la adhesión.
+                        <br />
+                        <br />
+                        Al no tener núcleo definido, su material genético no está contenido dentro de un núcleo separado. En cambio, el material genético está contenido dentro de la región citoplasmática conocida como nucleoide.
+                    </p>
+                </CollapseInformation>
+
+                <hr />
+
+                <CollapseInformation name="Organelos celulares" id="organelos">
+                    <CollapseInformation name="Mitocondria" id="mitocondria">
+                        <p>
+                            Son orgánelos celulares presentes en células eucariotas que se encargan de producir la mayor parte de la energía que necesita la célula a través del proceso de respiración celular. Están formadas por dos membranas (una externa y otra interna) y contienen su propio material genético, el ADN mitocondrial. Las mitocondrias se encuentran en casi todas las células eucariotas, incluyendo células musculares y neuronas, y se heredan exclusivamente de la madre en la mayoría de los organismos.
+                        </p>
+                    </CollapseInformation>
+                    <hr />
+                    <CollapseInformation name="Retículo endoplásmico" id="reticuloEndoplasmico">
+                        <p>
+                            El retículo endoplásmico puede ser liso o rugoso dependiendo de la presencia de ribosomas unidos a su superficie. El RE liso se encarga de la síntesis de lípidos y de la detoxificación celular, mientras que el RE rugoso se encarga de la síntesis de proteínas que serán secretadas o insertadas en la membrana celular.
+                        </p>
+                    </CollapseInformation>
+                    <hr />
+                    <CollapseInformation name="Aparato de Golgi" id="aparatoGolgi">
+                        <p>
+                            Es un organelo celular presente en las células eucariotas que se encarga de la modificación, clasificación, empaquetamiento y distribución de proteínas y lípidos que han sido sintetizados en el retículo endoplásmico. También se encarga de la síntesis de carbohidratos complejos.
+                        </p>
+                    </CollapseInformation>
+                    <hr />
+                    <CollapseInformation name="Lisosomas" id="lisosomas">
+                        <p>
+                            Organelos compuestos por enzimas digestivas capaces de degradar diversos materiales celulares, como proteínas, lípidos, carbohidratos, etc. Se encargan de la digestión y reciclaje de sustancias celulares, así como eliminar materiales externos a la célula que han sido fagocitados
+                        </p>
+                    </CollapseInformation>
+                    <hr />
+                    <CollapseInformation name="Ribosomas" id="Ribosomas">
+                        <p>
+                            Son organelos presentes en todas las células, encargados de la síntesis de proteínas. Están compuestos por ARN ribosomal y proteínas. Generalmente se encuentran libres en el citoplassma o unidos a un retículo endoplasmático rugoso. Durante la síntesis de proteínas, los ribosomas leen la información genética del ADN y la traducen en una secuencia de aminoácidos para formar la proteína correspondiente.
+                        </p>
+                    </CollapseInformation>
                 </CollapseInformation>
             </div>
         </div>
