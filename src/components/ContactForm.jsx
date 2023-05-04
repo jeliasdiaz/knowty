1f3b3396144112bc7872ede376c558b7a2fba0a1
Jose <diazmarinjoseelias2007@gmail.com>
2023-05-04 11:27:55 -0500

    knowty

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -21,7 +21,7 @@ export const ContactForm = () => {
     const [email, setEmail] = useState("")
     const [emailError, setEmailError] = useState("");
     const onEmail = (e) => {
-        const mailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const mailValidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
       
         const enteredEmail = e.target.value;
         setEmail(enteredEmail);
