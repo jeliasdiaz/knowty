9062cfc326af880805d95e302f4a80d156c95e9c
Jose <diazmarinjoseelias2007@gmail.com>
2023-07-06 09:14:20 -0500

    knowty

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -24,7 +24,7 @@ export const SubjectCard = ({ path, img, title, marginTop, animationDelay }) => 
         <>
             <Link to={path} className="text-decoration-none text-black d-none d-sm-block" data-aos="fade-down" data-aos-duration={animationDelay} data-aos-once="true" onClick={handleClick}>
                 <div className={`subjectLink d-flex ownShadow-sm ${marginTop} animation`} onClick={transitionPage}>
-                    <img src={`/img/${img}Icon.png`} alt="Subject img" className=" pe-2 animation" />
+                    <img src={`/img/${img}Icon.png`} alt="Subject img" className=" pe-2 animation" loading="lazy"/>
                     <h5 className="ms-2 animation">{title}</h5>
                     <BiChevronRight size={45} className="BiChevronRight animation" />
                 </div>
@@ -32,7 +32,7 @@ export const SubjectCard = ({ path, img, title, marginTop, animationDelay }) => 
 
             <Link to={path} className="text-decoration-none text-black d-block d-sm-none" onClick={handleClick}>
                 <div className={`subjectLink d-flex ownShadow ${marginTop} animationMobile`} data-aos="fade-down" data-aos-duration={animationDelay} data-aos-once="true">
-                    <img src={`/img/${img}Icon.png`} alt="Subject img" className="w-25 pe-2 animation" />
+                    <img src={`/img/${img}Icon.png`} alt="Subject img" className="w-25 pe-2 animation" loading="lazy" />
                     <h5 className="ms-2 animation">{title}</h5>
                     <BiChevronRight size={45} className="BiChevronRight animation" />
                 </div>
